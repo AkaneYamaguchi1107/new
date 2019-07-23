52898150d46c40babb21e50fa78204f5086b1ccc
user <user@user-no-MacBook-Pro.local>
2019-07-23 15:25:23 +0900

    heroセクション,ヘッダー、フッター実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,53 @@
 <template>
   <div>
+    <header class="header">
+      <p class="header__title">
+        <nuxt-link to="/">yamashia</nuxt-link>
+      </p>
+      <ul class="gnav">
+        <li class="gnav__item">
+          <nuxt-link to="/about" class="gnav__link">会社案内</nuxt-link>
+        </li>
+        <li class="gnav__item">
+          <nuxt-link to="/services" class="gnav__link">事業案内</nuxt-link>
+        </li>
+        <li class="gnav__item">
+          <nuxt-link to="/contact" class="gnav__link">お問合わせ</nuxt-link>
+        </li>
+      </ul>
+    </header>
     <nuxt />
+    <footer class="footer">
+      <p class="footer__copyright">&copy; yamashia</p>
+    </footer>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<style lang="scss" scoped>
+.header {
+  padding: 16px;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(black, 0.12);
+  &__title {
+    font-size: 32px;
+  }
 }
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+.gnav {
+  display: flex;
+  list-style: none;
+  &__link {
+    font-size: 16px;
+    padding-right: 32px;
+    display: inline;
+    &.nuxt-link-active {
+      font-weight: bold;
+    }
+  }
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.footer {
+  padding: 16px;
+  text-align: center;
+  font-size: 12px;
 }
 </style>
---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div class="heading">
+      <h2 class="heading__title">会社の理念</h2>
+      <p class="heading__sub-title">
+        御社利益のためのIT支援を致します
+        <br />臨機に時々の最善を尽くします
+      </p>
+    </div>
+  </div>
+</template>
+<style></style>
